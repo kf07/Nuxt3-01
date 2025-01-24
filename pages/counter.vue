98da84a806e2a06eedc2a60845a13d622fb58af4
kango furusu <furusu0715@gmail.com>
2025-01-24 22:28:20 +0900

    counter追加

---
+++ pages/counter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  const count = ref(1)
+  const double = computed(() => count.value * 2)
+  const increment = () => {
+    count.value++;
+  }
+</script>
+
+<template>
+  <p>{{count}}</p>
+  <p>{{double}}</p>
+  <button @click="increment">increment</button>
+</template>
